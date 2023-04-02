1bf105020af0b4616b940ffd0890fce0d2ece4c2
Jakob Gimpelj <22889949+Gimpely@users.noreply.github.com>
2023-04-02 21:05:10 +0200

    Delete lines.js

--- lines.js
+++
@@ -1,7 +0,0 @@
-const homeTitle = document.querySelector("#home h2");
-
-function addShowLinesClass() {
-  homeTitle.classList.add("show-lines");
-}
-
-homeTitle.addEventListener("animationend", addShowLinesClass);
